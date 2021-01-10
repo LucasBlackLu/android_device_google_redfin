1db3011bf3c7e070061484279f508969e31974da
Alessandro Astone <ales.astone@gmail.com>
2021-01-10 19:51:54 +0100

    redfin: Remove prebuilt kernel logic
    Change-Id: I97ba038b22f1e54493045c824397b429e9225a01

---
+++ derp_redfin.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/redfin/aosp_redfin.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 5
+PRODUCT_NAME := derp_redfin
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=redfin \
+    PRIVATE_BUILD_DESC="redfin-user 14 UP1A.231105.001 10817346 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:14/UP1A.231105.001/10817346:user/release-keys
