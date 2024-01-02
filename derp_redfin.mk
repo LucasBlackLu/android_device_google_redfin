0f15bd8dbc8c36728ea37249ec27e9e716c119a4
LucasBlackLu <lucasblack997@gmail.com>
2024-01-02 11:10:10 +0800

    redfin: Derpfest Bringup

--- derp_redfin.mk
+++ derp_redfin.mk
@@ -4,20 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Derpfest stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/redfin/aosp_redfin.mk)
-$(call inherit-product, device/google/redbull/lineage_common.mk)
 
-include device/google/redfin/device-lineage.mk
+include device/google/redfin/device-custom.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5
-PRODUCT_NAME := lineage_redfin
+PRODUCT_NAME := derp_redfin
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
